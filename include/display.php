167bb9ab1034fd987ebe606b82fa9c7432242e19
Dru Clark <dclark@ucsd.edu>
2018-10-03 10:55:48 -0700

    Display updates.

--- include/display.php
+++ include/display.php
@@ -1,47 +1,89 @@
-<?php 
-
+<?php
 
 class Display{
     
     private $debug;
-  
+    
     /////////////////////////////////////////////
     public function __construct(){
-            
+        
     }
     
+    /////////////////////////////////
+    public function br() {
+        print '<br />';
+    }
+    
+    /////////////////////////////////
+    public function logo() {
+        print"<img src=".HEADER_LOGO.">";
+        
+    }
+    
+    /////////////////////////////////
     public function html($json) {
-                 
-        $logo="<img src=".HEADER_LOGO."><br> ";       
-        $data =  json_decode($json); 
-               
-        print_header(GET_TERM_HTML);
-        print $logo;
+        
+        $data =  json_decode($json);
+        print $this->jsonToTable($data, TRUE);
+        
+    }
+    
+    /////////////////////////////////
+    public function show_arrays($json) {
+        
+        $data =  json_decode($json);
+        
         foreach ($data as $key => $value) {
             foreach ($value as $key2 => $value2) {
+                
                 print"<pre>";
                 print_r($value2);
                 print"</pre>";
-                //$this->jsonToTable ($value);
+                
             }
-
+            
         }
         
-        print_footer(GET_TERM_HTML);
-
     }
     
     
-    
-    
-   //////////////////////////
-   // hmmm 
-    
-    function jsonToTable ($data)
+    /////////////////////////////////
+    public function print_footer($type)
     {
-        $table = '
-    <table class="json-table" width="100%">
-    ';
+        
+        $html_footer = "</head></html>";
+        
+        switch ($type) {
+            case 'html':
+                print $html_footer;
+                break;
+                
+                
+        }
+        
+    }
+    /////////////////////////////////
+    public function print_html_header()
+    {
+        print WEBPAGE_HEADER;
+        
+    }
+    
+    /////////////////////////////////
+    public function print_page_header()
+    {
+        print WEBPAGE_HTML_HEADER;
+        
+    }
+    
+    //////////////////////////
+    // hmmm
+    
+    function jsonToTable($data, $outer = NULL)
+    {
+        
+        
+        $table = '<table class="json-table">';
         foreach ($data as $key => $value) {
             $table .= '
         <tr valign="top">
@@ -59,7 +101,8 @@ class Display{
             ';
             }
             if (is_object($value) || is_array($value)) {
-                $table .= jsonToTable($value);
+                $table .= $this->jsonToTable($value);
+                // $table .= print_r($value, TRUE);
             } else {
                 $table .= $value;
             }
@@ -68,10 +111,12 @@ class Display{
         </tr>
         ';
         }
-        $table .= '
-    </table>
-    ';
+        
+        $table .= '</table>';
+        if(!$outer)  $table .= '<hr>';
+        
         return $table;
     }
+    /////////////////////////////////
     
 }
